a322b894c1c95028c8a24eccfd66e0c8ccaaf806
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-21 09:36:23 +0700

    Update TestScript.lua

--- TestScript.lua
+++ TestScript.lua
@@ -5,7 +5,7 @@ local LocalPlayer = Players.LocalPlayer
 local API_URL = "https://2cb8592c-0d94-4348-86b2-42d0bc9b841d-00-5tyyjf8nengg.sisko.replit.dev/check_hwid"
 local HWID = gethwid and gethwid() or "Unknown"
 
--- Gửi HWID lên API để kiểm tra
+-- Gửi HWID lên API để kiểm tra hoặc thêm
 local function checkHWID()
     local requestData = HttpService:JSONEncode({hwid = HWID})
     local success, response = pcall(function()
@@ -20,7 +20,7 @@ local function checkHWID()
         end
     end
 
-    print("❌ HWID không hợp lệ! Kick người chơi.")
+    print("❌ Không thể xác minh HWID! Kick khỏi game.")
     LocalPlayer:Kick("⚠️ HWID không hợp lệ!")
     return false
 end
@@ -28,6 +28,6 @@ end
 -- Nếu HWID hợp lệ, chạy script chính
 if checkHWID() then
     print("✅ Chạy script chính!")
-    getgenv().Language = "English" 
+    getgenv().Language = "English"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
 end

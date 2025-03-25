8acacd39ffa8e5ac620cfb586f99d3775034dadd
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-25 12:13:49 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,17 +1,18 @@
-local keysJsonUrl = "https://raw.githubusercontent.com/Phatdepzaicrystal/Key/refs/heads/main/keys.json"
+local keysJsonUrl = "https://raw.githubusercontent.com/Phatdepzaicrystal/Key/refs/heads/main/keys.json" 
 
 local success, keysData = pcall(function()
-    return loadstring("return " .. game:HttpGet(keysJsonUrl))()
+    return game:GetService("HttpService"):JSONDecode(game:HttpGet(keysJsonUrl))
 end)
 
 if success and type(keysData) == "table" then
-    if keysData[getgenv().Key] and keysData[getgenv().Key] > os.time() * 1000 then
-        print("✅ Key hợp lệ! Đang chạy script...")
-        getgenv().Language = "English"
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
-    else
-        print("❌ Invalid Key!")
+    for key, expiry in pairs(keysData) do
+        if string.find(key, "Vxeze-") and expiry > os.time() * 1000 then
+            print("✅ Key hợp lệ! Đang chạy script...")
+            getgenv().Language = "English"
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
+            break
+        end
     end
 else
-    print("❌ Error.Plz Wait Admin Fix!")
+    print("❌ Không tải được danh sách key!")
 end

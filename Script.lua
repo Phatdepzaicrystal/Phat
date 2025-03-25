b9e625ba33c02a9a620a3cc06452ce889a599def
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-25 11:58:45 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,5 @@
 local HttpService = game:GetService("HttpService")
 
--- Tải danh sách key từ GitHub (CẬP NHẬT đường dẫn đúng của bạn)
 local keysJsonUrl = "https://raw.githubusercontent.com/Phatdepzaicrystal/Phat/main/keys.json"
 
 local function loadKeys()
@@ -12,11 +11,10 @@ end
 
 local keys = loadKeys()
 
--- Kiểm tra xem có key không
 if getgenv().Key and keys[getgenv().Key] and keys[getgenv().Key] > os.time() * 1000 then
     print("✅")
     getgenv().Language = "English"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
 else
-    print("❌ Key không hợp lệ hoặc đã hết hạn!")
+    print("❌Invalid Key")
 end

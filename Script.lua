1081c83b0fd4a09a3a527eed057d66a1a6a0cb31
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-25 12:09:48 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,20 +1,17 @@
-local HttpService = game:GetService("HttpService")
-
 local keysJsonUrl = "https://raw.githubusercontent.com/Phatdepzaicrystal/Phat/main/keys.json"
 
-local function loadKeys()
-    local success, keysData = pcall(function()
-        return HttpService:JSONDecode(game:HttpGet(keysJsonUrl))
-    end)
-    return success and keysData or {}
-end
+local success, keysData = pcall(function()
+    return loadstring("return " .. game:HttpGet(keysJsonUrl))()
+end)
 
-local keys = loadKeys()
-
-if getgenv().Key and keys[getgenv().Key] and keys[getgenv().Key] > os.time() * 1000 then
-    print("✅")
-    getgenv().Language = "English"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
+if success and type(keysData) == "table" then
+    if keysData[getgenv().Key] and keysData[getgenv().Key] > os.time() * 1000 then
+        print("✅ Key hợp lệ! Đang chạy script...")
+        getgenv().Language = "English"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
+    else
+        print("❌ Invalid Key!")
+    end
 else
-    print("❌Invalid Key")
+    print("❌ Error.Plz Wait Admin Fix!")
 end

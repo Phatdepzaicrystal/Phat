cf6608790153d166e762b11cee6c56cd04299acf
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-27 19:36:46 +0700

    Update TestScript.lua

--- TestScript.lua
+++ TestScript.lua
@@ -6,18 +6,24 @@ if not key or key == "" then
     return
 end
 
-local http = game:HttpGet
-local api_url = "https://90b5e3ad-055e-4b22-851d-bd511d979dbc-00-3591ow60fhoft.riker.replit.dev/save_key?hwid=" .. hwid .. "&key=" .. key
+local httpService = game:GetService("HttpService")
+local api_url = "https://90b5e3ad-055e-4b22-851d-bd511d979dbc-00-3591ow60fhoft.riker.replit.dev/check_key?hwid=" .. hwid .. "&key=" .. key
 
 local success, response = pcall(function()
-    return http(api_url)
+    return httpService:GetAsync(api_url)
 end)
 
 if success then
-    local json = game:GetService("HttpService"):JSONDecode(response)
-    if json.status == "false" then
+    local json = httpService:JSONDecode(response)
+    if json.status == "true" then
+        -- ✅ Key hợp lệ -> Chạy script
+        getgenv().Language = "English"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
+    else
+        -- ❌ Key không hợp lệ -> Kick người chơi
         game.Players.LocalPlayer:Kick("❌ " .. json.message)
     end
 else
-    game.Players.LocalPlayer:Kick("🚫 Không thể kết nối đến máy chủ!")
+    -- 🚫 Lỗi kết nối đến API
+    game.Players.LocalPlayer:Kick("🚫 Không thể kết nối đến máy chủ kiểm tra key!")
 end

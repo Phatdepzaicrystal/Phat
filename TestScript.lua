6583dd2eb7ba23e0541403bfcc931ee39a5ce13e
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2025-03-29 11:01:55 +0700

    Update TestScript.lua

--- TestScript.lua
+++ TestScript.lua
@@ -1,28 +1,42 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+repeat
+    wait()
+until game:IsLoaded() and game.Players.LocalPlayer
 
-local http = game:HttpGet
 local player = game.Players.LocalPlayer
-local hwidApi = "https://c36d7cfb-6f3a-4e9a-841d-4e7bcdc7e592-00-33ro0chwsdbwl.kirk.replit.dev/storehwid" 
+local httpService = game:GetService("HttpService")
+local hwidApi = "https://c36d7cfb-6f3a-4e9a-841d-4e7bcdc7e592-00-33ro0chwsdbwl.kirk.replit.dev/storehwid"
 local keyFile = "https://raw.githubusercontent.com/Phatdepzaicrystal/Key/refs/heads/main/keys.json"
 
 local function getHWID()
-    return  gethwid and gethwid() or "Unknown"
+    return gethwid and gethwid() or "Unknown"
 end
 
 local function checkKey(key)
-    local success, keys = pcall(function()
-        return game.HttpService:JSONDecode(http(keyFile))
-    end)
-    if not success then return false end
-    return keys[key] ~= nil
+    local success, result =
+        pcall(
+        function()
+            return httpService:JSONDecode(game:HttpGet(keyFile))
+        end
+    )
+
+    if not success then
+        return false
+    end
+    return result[key] ~= nil
 end
 
 local function sendHWID(key)
     local hwid = getHWID()
-    local data = game.HttpService:JSONEncode({ key = key, hwid = hwid })
-    local response = http(hwidApi, "POST", data, true)
+    local data = httpService:JSONEncode({key = key, hwid = hwid})
 
-    return response:find('"success":true') ~= nil
+    local success, response =
+        pcall(
+        function()
+            return httpService:PostAsync(hwidApi, data, Enum.HttpContentType.ApplicationJson)
+        end
+    )
+
+    return success and response and response:find('"success":true') ~= nil
 end
 
 if not getgenv().Key or not checkKey(getgenv().Key) then
@@ -36,4 +50,4 @@ if not sendHWID(getgenv().Key) then
 end
 
 getgenv().Language = "English"
-loadstring(http("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubMain2"))()
